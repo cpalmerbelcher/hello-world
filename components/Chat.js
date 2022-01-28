c00fbeac2655c9d293d461ddc75775069d0ec082
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2022-01-27 22:49:29 -0500

    chat updates
    trying to get the backgoround color to change from the start screen to the chat screen

--- components/Chat.js
+++ components/Chat.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 
 export default class Chat extends React.Component {
   render() {
+    // Set the background color selected from start screen
+    const { bgColor } = this.props.route.params;
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Welcome to Chat!</Text>
+      <View style={{
+        flex: 1,
+        alignItems:'center', 
+        justifyContent:'center', 
+        backgroundColor: bgColor ? bgColor : "#fff",}}>
+        <View style={styles.giftedChat}>
+           <GiftedChat
+              renderBubble={this.renderBubble.bind(this)}
+              renderInputToolbar={this.renderInputToolbar.bind(this)}
+              messages={this.state.messages}
+              user={this.state.user}
+              onSend={messages => this.onSend(messages)}
+              renderActions={this.renderCustomActions}
+              renderCustomView={this.renderCustomView}
+              user={{
+                _id: this.state.user._id,
+                name: this.state.name,
+                avatar: this.state.user.avatar
+              }}
+            />
+            { Platform.OS === 'android' ? (
+              <KeyboardAvoidingView behavior="height" />
+              ) : null}
+        </View>
       </View>
     )
   }
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems:'center', 
+    justifyContent:'center'
+  },
+  giftedChat: {
+    flex: 1,
+    width: "88%",
+    paddingBottom: 10,
+    justifyContent: "center",
+    borderRadius: 5,
+  },
+
+});
 
