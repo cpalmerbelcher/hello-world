02803a63425b21f7ac22e3cecf6b388429bc9a38
cpalmerbelcher <86631182+cpalmerbelcher@users.noreply.github.com>
2022-02-02 19:24:11 -0500

    updates to Chat.js
    added the chat room to the chat page where you can text back and forth with another user

--- components/Chat.js
+++ components/Chat.js
@@ -1,56 +1,87 @@
-import React from 'react';
-import { View, Text, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
-
+import React from "react";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { View, Platform, KeyboardAvoidingView, StyleSheet } from 'react-native';
 
 export default class Chat extends React.Component {
-  render() {
-    // Set the background color selected from start screen
-    const { bgColor } = this.props.route.params;
-    return (
-      <View style={{
-        flex: 1,
-        alignItems:'center', 
-        justifyContent:'center', 
-        backgroundColor: bgColor ? bgColor : "#fff",}}>
-        <View style={styles.giftedChat}>
-           {/* <GiftedChat
-              renderBubble={this.renderBubble.bind(this)}
-              renderInputToolbar={this.renderInputToolbar.bind(this)}
-              messages={this.state.messages}
-              user={this.state.user}
-              onSend={messages => this.onSend(messages)}
-              renderActions={this.renderCustomActions}
-              renderCustomView={this.renderCustomView}
-              user={{
-                _id: this.state.user._id,
-                name: this.state.name,
-                avatar: this.state.user.avatar
-              }}
-            /> */}
-            { Platform.OS === 'android' ? (
-              <KeyboardAvoidingView behavior="height" />
-              ) : null}
-        </View>
-      </View>
-    )
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
   }
 
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+         },
+      ],
+    });
+  }
+
+  //callback function when a user sends a message
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000',
+          },
+        }}
+      />
+    );
+  }
+
+  render() {
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
+    return (
+      <View style={{ flex: 1 }}>
+      <GiftedChat
+        renderBubble={this.renderBubble.bind(this)}
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      { Platform.OS === 'android' ? (
+      <KeyboardAvoidingView behavior="height" />
+       ) : null }
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems:'center', 
-    justifyContent:'center'
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
   },
   giftedChat: {
-    flex: 1,
-    width: "88%",
-    paddingBottom: 10,
-    justifyContent: "center",
-    borderRadius: 5,
+      color: '#000',
   },
-
 });
-
